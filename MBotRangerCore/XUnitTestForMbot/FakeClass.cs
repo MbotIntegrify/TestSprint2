8a0d24025a9c12d41bc6acb9db0f2b3566a1fe96
Daniel Mekonnen <daneymail@gmail.com>
2017-12-01 13:46:01 +0100

    Lastely Squashed

---
+++ MBotRangerCore/XUnitTestForMbot/FakeClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitMbot
+{
+    public class FakeClass
+    {
+        //First Comment
+        //Second comment
+        //Third Comment
+        public bool FakeMethod()
+        {
+            return true;
+        }
+    }
+}
