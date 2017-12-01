4d571ddb1c1979c70118e565986e31831dbefabb
Torbjörn Einegren <tobisintegrify@gmail.com>
2017-12-01 15:30:18 +0100

    Another push with comment

--- MBotRangerCore/MBotRangerCore/Controllers/MobileCamController.cs
+++ MBotRangerCore/MBotRangerCore/Controllers/MobileCamController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-// Tobis Comment 
+// Tobis Comment Again and Again
 
 namespace MBotRangerCore.Controllers
 {
